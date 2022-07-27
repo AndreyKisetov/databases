2b0eaea98696a0673022535c4fc5e019d879978e
Andrey Kisetov <rbcfff@yandex.ru>
2022-07-27 22:13:21 +0300

    added Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,15 @@
+﻿string text = String.Empty;
+
+int count = 50;
+string[] letters = {"o", "c", "e", "p"};
+
+Random random = new Random();
+for (int id = 1; id <= count; id++)
+{
+    int number = random.Next(100, 1000);
+    string letter = letters[random.Next(letters.Length)];
+    text += $"{id};\t{letter}{number}{letter}{letter}{" "}{11}\n";
+}
+
+// System.Console.WriteLine(text);
+File.WriteAllText("output.csv", text);
